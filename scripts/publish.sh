daca791cfeea35d52581b0bf47c9f85db6ff7a89
Charlie Sears <charlie@wildbit.com>
2019-05-27 16:51:03 -0400

    Travis CI fix, version bump

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -6,7 +6,7 @@ dotnet restore
 
 #generate a version, based on the tag, or rev.
 revision=$(git rev-parse HEAD)
-inferred_assembly_version="2.0.0.${TRAVIS_BUILD_NUMBER:-0}"
+inferred_assembly_version="3.0.1.${TRAVIS_BUILD_NUMBER:-0}"
 version=$(echo "${TRAVIS_TAG:-$inferred_assembly_version}" | sed -E s/-.+$//)
 descriptive_version=${TRAVIS_TAG:-"$version-git-${revision:0:6}"}
 
@@ -15,5 +15,5 @@ dotnet pack -c Release -o ../tmp ./UdgerParser/Udger.Parser.csproj -p:AssemblyVe
 
 package_path="./tmp/Udger.Parser.$descriptive_version.nupkg"
 echo "The package path is located at: $package_path"
-echo 'This package will be published to the CI "MyGet" feed
+echo 'This package will be published to the CI "MyGet" feed:'
 dotnet nuget push $package_path -s https://www.myget.org/F/postmark-ci/api/v3/index.json -k $MYGET_API_KEY
